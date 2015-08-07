386feec16d23899bb168119cd8007475ee8bcdc0
Nicola Asuni <nicola.asuni@datasift.com>
2015-08-07 09:54:26 +0100

    complete test code coverage for StepX

--- test/BidiTest.php
+++ test/BidiTest.php
@@ -69,7 +69,7 @@ class BidiTest extends \PHPUnit_Framework_TestCase
     /**
      * @dataProvider bidiStrDataProvider
      */
-    /*public function testBidiStr($str, $expected, $forcertl = false)
+    public function testBidiStr($str, $expected, $forcertl = false)
     {
         $bidi = new \Com\Tecnick\Unicode\Bidi($str, null, null, $forcertl);
         $this->assertEquals($expected, $bidi->getString());
@@ -79,91 +79,16 @@ class BidiTest extends \PHPUnit_Framework_TestCase
     {
         return array(
             array(
-                json_decode('"The words \"\u202e\u05de\u05d6\u05dc [mazel] '
-                    .'\u05d8\u05d5\u05d1 [tov]\u202c\" mean \"Congratulations!\""'),
-                'The words "[tov] בוט [mazel] לזמ" mean "Congratulations!"'
-            ),
-            array(
-                'اختبار بسيط',
-                'ﻂﻴﺴﺑ ﺭﺎﺒﺘﺧﺍ'
-            ),
-            array(
-                json_decode('"\u0671AB\u0679\u0683"'),
-                'ﭷﭩABٱ'
-            ),
-            array(
-                json_decode('"\u067137\u0679\u0683"'),
-                'ﭷﭩ37ٱ'
-            ),
-            array(
-                json_decode('"AB\u0683"'),
-                'ABﭶ'
-            ),
-            array(
-                json_decode('"AB\u0683"'),
-                'ABﭶ',
-                'L'
-            ),
-            array(
-                json_decode('"AB\u0683"'),
-                'ﭶAB',
-                'R'
-            ),
-            array(
-                json_decode('"he said \"\u0671\u0679! \u0683\" to her"'),
-                'he said "ﭧٱ! ﭶ" to her'
-            ),
-            array(
-                json_decode('"he said \"\u0671\u0679!\" to her"'),
-                'he said "ﭧٱ!" to her'
-            ),
-            array(
-                json_decode('"he said \"\u0671\u0679! \u200F\" to her"'),
-                'he said "ﭧٱ! ‏" to her'
-            ),
-            array(
-                json_decode('"START CODES \u202bRLE\u202a LRE \u202eRLO\u202d LRO \u202cPDF\u202c END"'),
-                'START CODES RLE LRE FDP LRO OLR END'
-            ),
-            array(
                 json_decode('"\u202EABC\u202C"'),
                 'CBA'
             ),
-            array(
-                json_decode('"\u202D\u0671\u0679\u0683\u202C"'),
-                'ٱﭩﭷ'
-            ),
-            array(
-                json_decode('"START RLE'
-                    .'\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b'
-                    .'\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b'
-                    .'\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b'
-                    .'\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b'
-                    .'\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b'
-                    .'\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b'
-                    .'\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b'
-                    .'\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b'
-                    .'\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b'
-                    .'\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b'
-                    .'\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b'
-                    .'\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b'
-                    .'\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b'
-                    .'\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b'
-                    .'\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b'
-                    .'\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b'
-                    .'\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b'
-                    .'\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b'
-                    .'\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b\u202b'
-                    .'"'),
-                'START RLE'
-            ),
         );
-    }*/
+    }
 
     /**
      * @dataProvider bidiOrdDataProvider
      */
-    /*public function testBidiOrd($ordarr, $expected, $forcertl = false)
+    public function testBidiOrd($ordarr, $expected, $forcertl = false)
     {
         $bidi = new \Com\Tecnick\Unicode\Bidi(null, null, $ordarr, $forcertl);
         $this->assertEquals($expected, $bidi->getOrdArray());
@@ -174,86 +99,9 @@ class BidiTest extends \PHPUnit_Framework_TestCase
     {
         return array(
             array(
-                array(1649,65,66,1657,1667),
-                array(64375,64361,65,66,1649),
-            ),
-            array(
-                array(1667,1657,65,66,1649),
-                array(64337,65,66,64361,64376),
-            ),
-            array(
-                array(65,66,1667),
-                array(65,66,64374),
-            ),
-            array(
-                array(65,66,1667),
-                array(65,66,64374),
-                'L'
-            ),
-            array(
-                array(65,66,1667),
-                array(64374,65,66),
-                'R'
-            ),
-            array(
-                array(917760,917761,917762,917763),
-                array(917763,917762,917761,917760),
-                'R'
-            ),
-            array(
-                array(48,8314,48,8314,48,8314),
-                array(8314,48,8314,48,8314,48),
-                'R'
-            ),
-            array(
-                array(1632,8239,1632,8239,1632,8239),
-                array(8239,1632,8239,1632,8239,1632),
-                'R'
-            ),
-            array(
-                array(1667,1657,65,66,48,162,49,50,1649),
-                array(64337,65,66,48,162,49,50,64361,64376),
-            ),
-            array(
-                array(65,66,1636,1637,1667,8233),
-                array(65,66,64374,1636,1637,8233),
-            ),
-            array(
-                // BS13 Example 1: text1·RLE·text2·PDF·RLE·text3·PDF·text4
-                array(33,8235,34,8236,8235,38,8236,39),
-                array(),
-                'L'
-            ),
-            array(
-                // BS13 Example 2: text1·RLI·text2·PDI·RLI·text3·PDI·text4
-                array(33,8295,34,8297,8295,38,8297,39),
-                array(),
-                'L'
-            ),
-            array(
-                // BS13 Example 3: text1·RLI·text2·LRI·text3·RLE·text4·PDF·text5·PDI·text6·PDI·text7
-                array(33,8295,34,8294,38,8235,39,8236,40,8297,41,8297,42),
-                array(),
-                'L'
-            ),
-            array(
-                // X10 Example 1: text1·RLE·text2·LRI·text3·RLE·text4·PDI·text5·PDF·text6
-                array(33,8235,34,8294,38,8235,39,8297,40,8236,41),
-                array(),
-                'L'
-            ),
-            array(
-                // X10 Example 2: text1·RLI·text2·LRI·text3·PDI·text4·PDI·RLI·text5·PDI·text6
-                array(33,8295,34,8294,38,8297,39,8297,8295,40,8297,41),
-                array(),
-                'L'
-            ),
-            array(
-                // X10 Example 3: text1·RLE·text2·LRE·text3·PDF·text4·PDF·RLE·text5·PDF·text6
-                array(33,8235,34,8234,38,8236,39,8236,8235,40,8236,41),
-                array(),
-                'L'
+                array(65),
+                array(65),
             ),
         );
-    }*/
+    }
 }
--- test/StepXTest.php
+++ test/StepXTest.php
@@ -36,9 +36,9 @@ class StepXTest extends \PHPUnit_Framework_TestCase
     /**
      * @dataProvider stepXDataProvider
      */
-    public function testStepX($ordarr, $expected)
+    public function testStepX($ordarr, $pel, $expected)
     {
-        $stepx = new \Com\Tecnick\Unicode\Bidi\StepX($ordarr, 0);
+        $stepx = new \Com\Tecnick\Unicode\Bidi\StepX($ordarr, $pel);
         $this->assertEquals($expected, $stepx->getChrData());
     }
 
@@ -48,6 +48,7 @@ class StepXTest extends \PHPUnit_Framework_TestCase
             array(
                 // BD13 Example 1: text1·RLE·text2·PDF·RLE·text3·PDF·text4
                 array(33,8235,34,8236,8235,38,8236,39),
+                0,
                 array(
                     array('char' => 33,   'level' => 0, 'type' => 'ON', 'otype' => 'ON'),
                     array('char' => 34,   'level' => 1, 'type' => 'ON', 'otype' => 'ON'),
@@ -58,6 +59,7 @@ class StepXTest extends \PHPUnit_Framework_TestCase
             array(
                 // BD13 Example 2: text1·RLI·text2·PDI·RLI·text3·PDI·text4
                 array(33,8295,34,8297,8295,38,8297,39),
+                0,
                 array(
                     array('char' => 33,   'level' => 0, 'type' => 'ON', 'otype' => 'ON'),
                     array('char' => 8295, 'level' => 0, 'type' => 'NI', 'otype' => 'NI'),
@@ -72,6 +74,7 @@ class StepXTest extends \PHPUnit_Framework_TestCase
             array(
                 // BD13 Example 3: text1·RLI·text2·LRI·text3·RLE·text4·PDF·text5·PDI·text6·PDI·text7
                 array(33,8295,34,8294,38,8235,39,8236,40,8297,41,8297,42),
+                0,
                 array(
                     array('char' => 33,   'level' => 0, 'type' => 'ON', 'otype' => 'ON'),
                     array('char' => 8295, 'level' => 0, 'type' => 'NI', 'otype' => 'NI'),
@@ -89,6 +92,7 @@ class StepXTest extends \PHPUnit_Framework_TestCase
             array(
                 // X10 Example 1: text1·RLE·text2·LRE·text3·PDF·text4·PDF·RLE·text5·PDF·text6
                 array(33,8235,34,8234,38,8236,39,8236,8235,40,8236,41),
+                0,
                 array(
                     array('char' => 33,   'level' => 0, 'type' => 'ON', 'otype' => 'ON'),
                     array('char' => 34,   'level' => 1, 'type' => 'ON', 'otype' => 'ON'),
@@ -101,6 +105,7 @@ class StepXTest extends \PHPUnit_Framework_TestCase
             array(
                 // X10 Example 2: text1·RLI·text2·LRI·text3·PDI·text4·PDI·RLI·text5·PDI·text6
                 array(33,8295,34,8294,38,8297,39,8297,8295,40,8297,41),
+                0,
                 array(
                     array('char' => 33,   'level' => 0, 'type' => 'ON', 'otype' => 'ON'),
                     array('char' => 8295, 'level' => 0, 'type' => 'NI', 'otype' => 'NI'),
@@ -119,6 +124,7 @@ class StepXTest extends \PHPUnit_Framework_TestCase
             array(
                 // X10 Example 3: text1·RLE·text2·LRI·text3·RLE·text4·PDI·text5·PDF·text6
                 array(33,8235,34,8294,38,8235,39,8297,40,8236,41),
+                0,
                 array(
                     array('char' => 33,   'level' => 0, 'type' => 'ON', 'otype' => 'ON'),
                     array('char' => 34,   'level' => 1, 'type' => 'ON', 'otype' => 'ON'),
@@ -128,6 +134,79 @@ class StepXTest extends \PHPUnit_Framework_TestCase
                     array('char' => 8297, 'level' => 1, 'type' => 'NI', 'otype' => 'NI'),
                     array('char' => 40,   'level' => 1, 'type' => 'ON', 'otype' => 'ON'),
                     array('char' => 41,   'level' => 0, 'type' => 'ON', 'otype' => 'ON')
+                )
+            ),
+            array(
+                // text1·RLO·text2·LRO·text3·RLO·text4·PDF·text5·PDF·text6
+                array(33,8238,34,8237,38,8238,39,8236,40,8236,41),
+                0,
+                array(
+                    array('char' => 33,   'level' => 0, 'type' => 'ON', 'otype' => 'ON'),
+                    array('char' => 34,   'level' => 1, 'type' => 'R',  'otype' => 'ON'),
+                    array('char' => 38,   'level' => 2, 'type' => 'L',  'otype' => 'ON'),
+                    array('char' => 39,   'level' => 3, 'type' => 'R',  'otype' => 'ON'),
+                    array('char' => 40,   'level' => 2, 'type' => 'L',  'otype' => 'ON'),
+                    array('char' => 41,   'level' => 1, 'type' => 'R',  'otype' => 'ON')
+                )
+            ),
+            array(
+                // text1·FSI·text2·PDI·text3
+                array(33,8296,34,8297,38),
+                0,
+                array(
+                    array('char' => 33,   'level' => 0, 'type' => 'ON', 'otype' => 'ON'),
+                    array('char' => 8296, 'level' => 0, 'type' => 'NI', 'otype' => 'NI'),
+                    array('char' => 34,   'level' => 2, 'type' => 'ON', 'otype' => 'ON'),
+                    array('char' => 8297, 'level' => 0, 'type' => 'NI', 'otype' => 'NI'),
+                    array('char' => 38,   'level' => 0, 'type' => 'ON', 'otype' => 'ON'),
+                )
+            ),
+            array(
+                // text1·FSI·text2·PDI·text3
+                array(1488,8296,1489,8297,1490),
+                1,
+                array(
+                    array('char' => 1488, 'level' => 1, 'type' => 'R',  'otype' => 'R'),
+                    array('char' => 8296, 'level' => 1, 'type' => 'NI', 'otype' => 'NI'),
+                    array('char' => 1489, 'level' => 3, 'type' => 'R',  'otype' => 'R'),
+                    array('char' => 8297, 'level' => 1, 'type' => 'NI', 'otype' => 'NI'),
+                    array('char' => 1490, 'level' => 1, 'type' => 'R',  'otype' => 'R'),
+                )
+            ),
+            array(
+                // text1·BN·text2·BN·text3
+                array(33,1807,34,1807,38),
+                0,
+                array(
+                    array('char' => 33, 'level' => 0, 'type' => 'ON', 'otype' => 'ON'),
+                    array('char' => 34, 'level' => 0, 'type' => 'ON', 'otype' => 'ON'),
+                    array('char' => 38, 'level' => 0, 'type' => 'ON', 'otype' => 'ON'),
+                )
+            ),
+            array(
+                // Test overflow: text1·130xLRE·LRI·PDF·PDI·PDF·PDI·text2
+                array(
+                    33,
+                    8234,8234,8234,8234,8234,8234,8234,8234,8234,8234,
+                    8234,8234,8234,8234,8234,8234,8234,8234,8234,8234,
+                    8234,8234,8234,8234,8234,8234,8234,8234,8234,8234,
+                    8234,8234,8234,8234,8234,8234,8234,8234,8234,8234,
+                    8234,8234,8234,8234,8234,8234,8234,8234,8234,8234,
+                    8234,8234,8234,8234,8234,8234,8234,8234,8234,8234,
+                    8234,8234,8234,8234,8234,8234,8234,8234,8234,8234,
+                    8234,8234,8234,8234,8234,8234,8234,8234,8234,8234,
+                    8234,8234,8234,8234,8234,8234,8234,8234,8234,8234,
+                    8234,8234,8234,8234,8234,8234,8234,8234,8234,8234,
+                    8234,8234,8234,8234,8234,8234,8234,8234,8234,8234,
+                    8234,8234,8234,8234,8234,8234,8234,8234,8234,8234,
+                    8294,8236,8297,8236,8297,
+                    34
+                ),
+                0,
+                array(
+                    array('char' => 33,   'level' => 0,   'type' => 'ON', 'otype' => 'ON'),
+                    array('char' => 8294, 'level' => 124, 'type' => 'NI', 'otype' => 'NI'),
+                    array('char' => 34,   'level' => 124, 'type' => 'ON', 'otype' => 'ON'),
                 )
             ),
         );

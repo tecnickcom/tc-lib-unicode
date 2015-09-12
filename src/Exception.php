5423f0a42ad0d0e60a348fff0bd404fe2a6f8a76
nicolaasuni <info@tecnik.com>
2015-09-12 10:50:07 +0100

    fix project links and dependencies

--- src/Exception.php
+++ src/Exception.php
@@ -8,7 +8,7 @@
  * @author      Nicola Asuni <info@tecnick.com>
  * @copyright   2015-2015 Nicola Asuni - Tecnick.com LTD
  * @license     http://www.gnu.org/copyleft/lesser.html GNU-LGPL v3 (see LICENSE.TXT)
- * @link        https://github.com/tecnick.com/tc-lib-file
+ * @link        https://github.com/tecnickcom/tc-lib-file
  *
  * This file is part of tc-lib-unicode software library.
  */
@@ -26,7 +26,7 @@ namespace Com\Tecnick\Unicode;
  * @author      Nicola Asuni <info@tecnick.com>
  * @copyright   2015-2015 Nicola Asuni - Tecnick.com LTD
  * @license     http://www.gnu.org/copyleft/lesser.html GNU-LGPL v3 (see LICENSE.TXT)
- * @link        https://github.com/tecnick.com/tc-lib-file
+ * @link        https://github.com/tecnickcom/tc-lib-file
  */
 class Exception extends \Exception
 {

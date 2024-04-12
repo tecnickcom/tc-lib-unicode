55b48356a7fdba80197dee14495c225b6fed7ad1
nicolaasuni <info@tecnick.com>
2024-04-12 05:56:01 +0100

    Update dependencies

--- resources/rpm/rpm.spec
+++ resources/rpm/rpm.spec
@@ -18,7 +18,7 @@ BuildArch: noarch
 
 Requires:  php(language) >= 8.0.0
 Requires:  php-composer(%{c_vendor}/tc-lib-unicode-data) < 2.0.0
-Requires:  php-composer(%{c_vendor}/tc-lib-unicode-data) >= 2.0.10
+Requires:  php-composer(%{c_vendor}/tc-lib-unicode-data) >= 2.0.11
 Requires:  php-pcre
 Requires:  php-mbstring
 
@@ -46,5 +46,5 @@ rm -rf $RPM_BUILD_ROOT
 #%config(noreplace) %{_configpath}*
 
 %changelog
-* Tue Jul 21 2015 Nicola Asuni <info@tecnick.com> 1.0.0-1
+* Tue Jul 21 2024 Nicola Asuni <info@tecnick.com> 1.0.0-1
 - Initial Commit
--- src/Exception.php
+++ src/Exception.php
@@ -7,7 +7,7 @@
  * @category  Library
  * @package   File
  * @author    Nicola Asuni <info@tecnick.com>
- * @copyright 2015-2023 Nicola Asuni - Tecnick.com LTD
+ * @copyright 2015-2024 Nicola Asuni - Tecnick.com LTD
  * @license   http://www.gnu.org/copyleft/lesser.html GNU-LGPL v3 (see LICENSE.TXT)
  * @link      https://github.com/tecnickcom/tc-lib-file
  *
@@ -25,7 +25,7 @@ namespace Com\Tecnick\Unicode;
  * @category  Library
  * @package   File
  * @author    Nicola Asuni <info@tecnick.com>
- * @copyright 2015-2023 Nicola Asuni - Tecnick.com LTD
+ * @copyright 2015-2024 Nicola Asuni - Tecnick.com LTD
  * @license   http://www.gnu.org/copyleft/lesser.html GNU-LGPL v3 (see LICENSE.TXT)
  * @link      https://github.com/tecnickcom/tc-lib-file
  */
